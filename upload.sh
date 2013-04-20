54840390a347959a55c59af3332d0984d72b3e9d
Joel Griffith <joel@joel-griffith.com>
2013-04-19 22:08:11 -0400

    update upload to remove git files

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rm myAuction.tar
-tar cvf myAuction.tar .
+tar cvf myAuction.tar myauction myAuction.java
 scp myAuction.tar jdg39@unixs.cssd.pitt.edu:~/private/cs1555/group/myAuction.tar
